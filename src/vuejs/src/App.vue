eb03c99c6f56a7e1c9ffaddf7737b61f656213a3
mennan <mennankose@gmail.com>
2017-05-28 15:34:06 +0300

    Initial commit.

---
+++ src/vuejs/src/App.vue
@@ -0,0 +1,67 @@
+﻿<template>
+  <dtable :headerRows="headerRows"></dtable>
+</template>
+
+<script>
+  import Dtable from './plugins/dtable.vue';
+  export default {
+    name: 'app',
+    components: {
+      Dtable
+    },
+    data() {
+      return {
+        headerRows: [
+          {
+            class: "sl",
+            sort: false,
+            field: "",
+            filterType: "",
+            filterData: "",
+            text: ""
+          },
+          {
+            class: "",
+            sort: true,
+            field: "UserName",
+            filterType: "",
+            filterData: "",
+            text: "User Name"
+          },
+          {
+            class: "",
+            sort: false,
+            field: "",
+            filterType: "",
+            filterData: "",
+            text: "Display Name"
+          },
+          {
+            class: "",
+            sort: true,
+            field: "Email",
+            filterType: "",
+            filterData: "",
+            text: "Email"
+          },
+          {
+            class: "",
+            sort: true,
+            field: "CreatedDate",
+            filterType: "",
+            filterData: "",
+            text: "Created"
+          },
+          {
+            class: "",
+            sort: true,
+            field: "State",
+            filterType: "dropdown",
+            filterData: "filterUserStates",
+            text: "State"
+          }
+        ]
+      }
+    }
+  }
+</script>
---
+++ src/vuejs/src/plugins/dtable.vue
@@ -0,0 +1,26 @@
+﻿<template>
+  <table class="table table-hover table-striped table-condensed" id="users_table">
+    <thead class="th-style">
+      <tr>
+        <th v-for="headerRow in headerRows" :class="headerRow.class" :data-sort="headerRow.sort" :data-field="headerRow.field" :data-filter-type="headerRow.filterType" :data-filter-data="headerRow.filterData">{{ headerRow.text }}</th>
+      </tr>
+    </thead>
+  </table>
+</template>
+<script>
+  export default {
+    name: 'dtable',
+    props: {
+      headerRows: Array,
+      tableClass: String,
+      tableId: String
+    },
+    data() {
+      return {
+        headerRows: this.headerRows || [],
+        tableClass: this.tableClass || "table table-hover table-striped table-condensed",
+        tableId: this.tableId || ""
+      }
+    }
+  }
+</script>
